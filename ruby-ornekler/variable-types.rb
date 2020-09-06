1532d6c205d27f51d406be561a12ba28d35bfbd0
Muhammet Mert POLAT <mrtplt024@gmail.com>
2020-09-06 20:09:05 +0300

    Create variable-types.rb

---
+++ ruby-ornekler/variable-types.rb
@@ -0,0 +1,24 @@
+degisken  = "Değişken" # String Değişken
+degisken2 = "Değişken2" # String Değişken
+
+degisken_bir = "Değişken bir" # String Değişken
+degisken_iki = "Değişken iki" # String Değişken
+
+degiskenTanimlama = "Değişkenler Büyük Küçük Harf Duyarlıdır" # String Değişken
+degiskentanimlama = "Değişkenler Programlamanın Piyonlarıdır" # String Değişken
+
+yas = 18 # İnteger Değişken
+vergi = 0.175 # Float Değişken
+dogru = true # Boolean Değişken
+yanlis = false # Boolean Değişken
+
+puts degisken.class,
+	degisken2.class,
+	degisken_bir.class,
+	degisken_iki.class,
+	degiskenTanimlama.class,
+	degiskentanimlama.class,
+	yas.class,
+	vergi.class,
+	dogru.class,
+	yanlis.class
